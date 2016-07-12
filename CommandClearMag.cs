f22d564b2c19c8a03536fe39ecae35e0dc70917b
sharkbound <laptopblaster@gmail.com>
2016-07-11 18:37:34 -0700

    removed refence jenkins was complaining about

--- CommandClearMag.cs
+++ CommandClearMag.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Rocket.Unturned.Player;
 using SDG.Unturned;
 using Rocket.Unturned.Chat;
--- CommandCloneItem.cs
+++ CommandCloneItem.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using fr34kyn01535.Uconomy;
 using SDG.Unturned;
 
